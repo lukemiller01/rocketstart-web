297ff065be5169a847948a9b0c3f983d1d1ed793
Luke Miller <lam59@buffalo.edu>
2022-12-18 17:54:27 -0500

    Removed warnings, which are treated as errors in Netlify.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header, Process, Questions, Testimonials, Footer } from '../../containers';
+// import { Header, Process, Questions, Testimonials, Footer } from '../../containers';
 import Navbar from '../../components/navbar/Navbar'
 
 
