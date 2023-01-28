d5f1dd62c3578c3fd943ed22425c481f1d74532c
Luke Miller <lam59@buffalo.edu>
2023-01-27 23:22:19 -0500

    Added "on load" animation to header container.

--- client/src/containers/header/Header.jsx
+++ client/src/containers/header/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import './header.css'
-import headerDemo from '../../assets/header-demo.mp4'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './header.css';
+import headerDemo from '../../assets/header-demo.mp4';
+import { Link } from 'react-router-dom';
 
-const header = () => {
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+// import 'animate.css';
+
+const Header = () => {
 
   function buttonClick() {
     window.umami.trackEvent('Header Button');
@@ -12,48 +15,50 @@ const header = () => {
   return (
     <div className="header">
       <div className="header__content-container">
-        <div className="left__content">
-          <h1 className='header__content-title'>
-            <font className='color__change'>Stand Out </font>
-            In Every Job Application.
-          </h1>
+        <div className='left__content'>
+          <AnimationOnScroll animateOnce={true} animateIn='fadeInUp' duration={1}>
+            <h1 className='header__content-title'>
+              <font className='color__change'>Stand Out </font>
+              In Every Job Application.
+            </h1>
+          </AnimationOnScroll>
           <div className='cta__benefit-container'>
-            <div className='cta__benefits'>
-                <span className="material-icons header__check">check_circle</span>
-                <p className='cta__text'>
-                  Discover the recruiter & hiring manager for 
-                  <font className='weight__change'> any </font>
-                  job
-                </p>
-            </div>
-            <div className='cta__benefits'>
-                <span className="material-icons header__check">check_circle</span>
-                <p className='cta__text'>
-                  Maximize reply rates on LinkedIn with text analysis
-                </p>
-            </div>
-            <div className='cta__benefits'>
-                <span className="material-icons header__check">check_circle</span>
-                 <p className='cta__text'>Receive contact suggestions until you make a connection</p>
-            </div>
-        </div>
-          <div className='button__container'>
+              <AnimationOnScroll animateOnce={true} duration={1} animateIn='fadeInLeft' delay={100} className='cta__benefits'>
+                  <span className="material-icons header__check">check_circle</span>
+                  <p className='cta__text'>
+                    Discover the recruiter & hiring manager for 
+                    <font className='weight__change'> any </font>
+                    job
+                  </p>
+              </AnimationOnScroll>
+              <AnimationOnScroll animateOnce={true} duration={1} animateIn='fadeInLeft' delay={200} className='cta__benefits'>
+                  <span className="material-icons header__check">check_circle</span>
+                  <p className='cta__text'>
+                    Maximize reply rates on LinkedIn with text analysis
+                  </p>
+              </AnimationOnScroll>
+              <AnimationOnScroll animateOnce={true} duration={1} animateIn='fadeInLeft' delay={300} className='cta__benefits'>
+                  <span className="material-icons header__check">check_circle</span>
+                  <p className='cta__text'>Receive contact suggestions until you make a connection</p>
+              </AnimationOnScroll>
+          </div>
+          <AnimationOnScroll animateOnce={true} offset={1000} duration={1} animateIn='fadeInDown' className='button__container'>
             <Link to='/login'>
               <button className='button' onClick={buttonClick}>
                 Get started for free
               </button>
             </Link>
-          </div>
+          </AnimationOnScroll>
         </div>
-        <div className='right__content' >
-          <video width="100%" height="100%" autoPlay playsInline loop muted className='rocketstart__demo'>
+        <AnimationOnScroll animateOnce={true} animateIn='fadeInRight' duration={1} className='right__content' >
+          <video width="100%" height="100%" playsInline loop muted className='rocketstart__demo'>
             <source src={headerDemo} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
-        </div>
+        </AnimationOnScroll>
       </div>
     </div>
   )
 }
 
-export default header
+export default Header
